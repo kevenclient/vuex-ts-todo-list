b486525bef064c5093fa505c2e520cc60d414cde
DESKTOP-6LPAETK\Keven Client <kevenclient@outlook.com>
2022-08-29 21:07:18 +0800

    include bootstrap in this test project

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 
-createApp(App).use(store).mount('#app')
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const app = createApp(App);
+
+app.use(store);
+app.mount('#app');
