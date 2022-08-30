5aef81f0d4e942a5a74a2fa0b418b0435f4cf2cf
DESKTOP-6LPAETK\Keven Client <kevenclient@outlook.com>
2022-08-30 11:19:49 +0800

    create todo form

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card rounded-0 shadow mx-auto">
+    <div class="card-body">
+      <div class="form-floating">
+        <input type="text" class="form-control border-0 shadow-none"
+          id="todo" placeholder="What needs to be done?">
+        <label for="todo">What needs to be done?</label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TodoForm',
+});
+</script>
+
+<style scoped>
+
+</style>
