5ba59d5f2292a6a770e9632ebdd7fd015e2660d6
DESKTOP-6LPAETK\Keven Client <kevenclient@outlook.com>
2022-09-01 15:18:34 +0800

    add value binding to todo item checkbox

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,6 +1,6 @@
 <template>
   <li class="list-group-item fs-5 p-4 rounded-0 border-top-0 d-flex gap-3">
-    <input class="form-check-input flex-shrink-0" type="checkbox" :checked="closed"/>
+    <input class="form-check-input flex-shrink-0" type="checkbox" v-model="closed"/>
     <label class="form-check-label" :class="statusClass">
       {{ item?.description }}
     </label>
@@ -18,10 +18,17 @@ export default defineComponent({
     item: Object as PropType<TodoItemInterface>,
   },
   computed: {
-    closed() {
-      return this.item?.status === Status.CLOSED;
+    closed: {
+      get(): boolean {
+        return this.item?.status === Status.CLOSED;
+      },
+      set(value: boolean): void {
+        const item = this.item as TodoItemInterface;
+        item.status = value 
+          ? Status.CLOSED : Status.OPEN;
+      }
     },
-    statusClass() {
+    statusClass(): object {
       return {
         'text-decoration-line-through': this.closed,
       };
